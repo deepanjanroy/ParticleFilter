e74375ee9c5df1da5104ba0654578d6a17f7e2b4
Deepanjan Roy <deepanjan.roy@mail.mcgill.ca>
2013-12-02 19:45:02 -0500

    Sets map loading from the command line
    Also cleaned up the main pfilter file a bit.

--- pfilter/scripts/pfilter.py
+++ pfilter/scripts/pfilter.py
@@ -1,8 +1,16 @@
+#!/usr/bin/env python
+
 import roslib
 roslib.load_manifest('pfilter')
 import rospy
+import sys
+import cv2
+
 from std_msgs.msg import String
 from nav_msgs.msg import Odometry
+
+from conversion import heading_from_qt
+from pfilter_exceptions import ImageLoadError
 
 class Particle:
 
@@ -18,32 +26,80 @@ class Particle:
     def draw(self):
         pass
 
+
 class ParticleFilter:
+
+    def sync_callback(self):
+        pass
     
-
-
-    def __init__(map_path, num_particles, eval_degree):
+    def register_listeners(self):
         pass
 
-    def spin():
+    def __init__(self, map_path, num_particles, eval_degree):
+        self.map = cv2.imread(map_path, cv2.CV_LOAD_IMAGE_GRAYSCALE)
+        if self.map == None:
+            raise ImageLoadError
+
+        cv2.namedWindow("Particle filter")
+
+        rospy.init_node('particle_filter', anonymous=True)
+        self.register_listeners()
+
+    def spin(self):
+        while not rospy.is_shutdown():
+            self.propagate()
+            self.update()
+            self.resample()
+
+            cv2.imshow("Particle filter", self.map)
+            cv2.waitKey(1)
+
+    def plot_particle(self, particle):
         pass
-        # The main loop
 
+    def plot_all_particles(self):
+        pass
+
+    def clear_particles(self):
+        pass
+
+    def replot(self):
+        pass
+
+    def resample(self):
+        pass
+
+    def in_obstacle(self):
+        pass
+
+    def propagate(self, dx, dy, dh):
+        pass
+
+    def update_weights(self):
+        pass
+
+
+def main():
+    args = sys.argv
+    usage = "Usage = rosrun pfilter pfilter.py [png map image path]"
     
+    if len(args) != 2:
+        print usage
+        return
+    
+    try:
+        pf = ParticleFilter(args[1], num_particles=1000, eval_degree=1)
+    except ImageLoadError:
+        print "Error loading map bitmap from " + args[1]
+        return
 
-
-
-if __name__ == "__main__": 
-    pf = ParticleFilter()
     pf.spin()
 
-    c.listener()
-    while not rospy.is_shutdown():
-        try:
-            print c.heading
-            c.showimage()
-            # c.waitKey
-        except KeyboardInterrupt:
-            print "Bye"
-            sys.exit()
+
+if __name__ == "__main__":
+    main()
+
+
+
+
 
---
+++ pfilter/scripts/pfilter_exceptions.py
@@ -0,0 +1,2 @@
+class ImageLoadError(Exception):
+    pass
